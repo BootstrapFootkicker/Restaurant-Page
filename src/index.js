244c94774899c54ff365046dd664ed77405d7f9e
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2023-03-04 19:12:58 -0700

    javascript Started code for javascript creation of page Commented out html code

--- src/index.js
+++ src/index.js
@@ -2,6 +2,36 @@ import './style.css'
 import _ from 'lodash';
 
 
+let contentDiv = document.createElement("div");
+let headerDiv = document.createElement("header");
+let headerNameDiv = document.createElement("div");
+let headerLinkDiv = document.createElement("ul");
+let headerLinkOne = document.createElement('li')
+let headerLinkTwo = document.createElement('li')
+
+contentDiv.classList.add('Content');
+headerDiv.classList.add('header-container');
+headerNameDiv.classList.add('name-div');
+headerLinkDiv.classList.add('header-links');
 
 
+/*header stuff*/
+
+headerNameDiv.innerText = 'WAFFLES 101';
+headerLinkOne.innerText = 'OUR MENU';
+headerLinkTwo.innerText = 'LOCATIONS';
+
+
+
+headerLinkDiv.appendChild(headerLinkOne);
+headerLinkDiv.appendChild(headerLinkTwo);
+headerDiv.appendChild(headerNameDiv);
+headerDiv.appendChild(headerLinkDiv)
+
+
+/*header stuff*/
+
+
+contentDiv.appendChild(headerDiv);
+document.body.appendChild(contentDiv);
 
