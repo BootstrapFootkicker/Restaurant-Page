d2111b09a48775cb092aa996523b6a1cacf7d8d7
BootstrapFootkicker <89753423+BootstrapFootkicker@users.noreply.github.com>
2023-03-11 22:27:19 -0700

    Create header Added function to create header

--- src/index.js
+++ src/index.js
@@ -1,37 +1,33 @@
 import './style.css'
 import _ from 'lodash';
 
+function createHeader() {
+    let contentDiv = document.createElement("div");
+    let headerDiv = document.createElement("header");
+    let headerNameDiv = document.createElement("div");
+    let headerLinkDiv = document.createElement("ul");
+    let headerLinkOne = document.createElement('li')
+    let headerLinkTwo = document.createElement('li')
 
-let contentDiv = document.createElement("div");
-let headerDiv = document.createElement("header");
-let headerNameDiv = document.createElement("div");
-let headerLinkDiv = document.createElement("ul");
-let headerLinkOne = document.createElement('li')
-let headerLinkTwo = document.createElement('li')
+    contentDiv.classList.add('Content');
+    headerDiv.classList.add('header-container');
+    headerNameDiv.classList.add('name-div');
+    headerLinkDiv.classList.add('header-links');
+    
 
-contentDiv.classList.add('Content');
-headerDiv.classList.add('header-container');
-headerNameDiv.classList.add('name-div');
-headerLinkDiv.classList.add('header-links');
-
-
-/*header stuff*/
-
-headerNameDiv.innerText = 'WAFFLES 101';
-headerLinkOne.innerText = 'OUR MENU';
-headerLinkTwo.innerText = 'LOCATIONS';
+    headerNameDiv.innerText = 'WAFFLES 101';
+    headerLinkOne.innerText = 'OUR MENU';
+    headerLinkTwo.innerText = 'LOCATIONS';
 
 
+    headerLinkDiv.appendChild(headerLinkOne);
+    headerLinkDiv.appendChild(headerLinkTwo);
+    headerDiv.appendChild(headerNameDiv);
+    headerDiv.appendChild(headerLinkDiv)
 
-headerLinkDiv.appendChild(headerLinkOne);
-headerLinkDiv.appendChild(headerLinkTwo);
-headerDiv.appendChild(headerNameDiv);
-headerDiv.appendChild(headerLinkDiv)
+    contentDiv.appendChild(headerDiv);
+    return contentDiv
+}
 
-
-/*header stuff*/
-
-
-contentDiv.appendChild(headerDiv);
-document.body.appendChild(contentDiv);
+document.body.appendChild(createHeader());
 
